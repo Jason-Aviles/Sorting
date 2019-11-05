a70a577b26f3720a40dec2bb984b25f1c6df252e
Jason Aviles <sandman2k18@outlook.com>
2019-11-05 15:37:53 -0500

    finnshed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,25 +2,34 @@
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
+        cur_index = i+1
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
+        for j in range(0, len(arr) - 1):
+               if arr[cur_index] < arr[j]:
+                temp = arr[j]
+                arr[j] = arr[cur_index] 
+                arr[smallest_index]= temp
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    
+    for i in range(0, len(arr) - 1):
+        noSwap= True
+        for j in range(0, len(arr)-1 ):
+            noSwap=False
+            if arr[j] > arr[j+1]:
+                swap = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = swap
+            if(noSwap):break
 
+            
+       
     return arr
 
 
@@ -28,3 +37,7 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+
+
+
